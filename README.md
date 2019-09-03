b92c39379a78557b02b8faeabf627af64e159c9a
abc123 <abc123@gmail.com>
2019-09-03 18:13:21 +1200

    Reorganize

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 # Behavior Driven Development CLI Generator
 
+## Introduction
+
 Given a yaml file defining commands with a particular syntax, generates a cli program.
+
+## Contents
+
+-   [Introduction](#introduction)
+-   [Contents](#contents)
+-   [Installation](#installation)
+-   [Usage](#usage)
+
+## Installation
+
+```shell
+// With yarn
+yarn add @mithray/bdd-cligen
+
+// With npm
+npm i @mithray/bdd-cligen
+```
+
+## Usage
+
+There are two steps to using this tool.
+
+1. Using the `bdd-cligen` module
+2. Writing a yaml file that defines your own API
+
+### Using the bdd-cligen module
+
+This is the main executable and should be placed in the `./bin` directory of your node project. The variable `program` is a [commander](https://www.npmjs.com/package/commander) object.
+
+```javascript
+#!/usr/bin/env node
+
+const cligen = require('@mithray/bdd-cligen')
+cligen({ path: './api.yml' }).then(program => {
+    program.parse(process.argv)
+})
+```
+
+### Writing a yaml file
+
+The yaml program specifies options defining the interface of your functions. Not all of these are necessary to generate the cli, but they could be used by other programs such as an integration with JSDoc andmocha testing. `bdd-cligen` only requires you to define the variables `description`, `function`, `parameter_desc` for every function. Testing might suggest the `path_prefix` also needs to be defined, and the root parent of the tests also must be called `api`.
 
 ```yaml
 ---
@@ -14,24 +57,11 @@ api:
         tests:
             - parameters: { someString: 'A description of various tests' }
               condition: 'should have property "api"'
-            - parameters:
+            - parameters: >
                   {
                       someString: 'These descriptions will be used to generate BDD tests using mochajs and should.js',
                   }
               condition: 'should have property "api"'
-            - parameters:
-                  { someString: 'different tests to test the named function' }
-              condition: 'should have property "api"'
-            - parameters:
-                  {
-                      someString: 'The syntax of the condition property is like that of should.js, ',
-                  }
-              condition: 'should have property "api"'
-            - parameters:
-                  {
-                      someString: 'except that it has dots converted to spaces and function calls converted to quotes',
-                  }
-              condition: 'should have property "api"'
 ```
-# bdd-cligen
-# bdd-cligen
+
+Note, the `tests` property will not actually be used, but it is likely to be helpful for future integrations. These test descriptions will be used to generate BDD tests using mochajs and should.js. The syntax of the condition property is like that of should.js, except that it has dots converted to spaces and function calls converted to quotes.
---
+++ index.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const path = require('path')
+const changeCase = require('change-case')
+const log = require('@mithray/logger')
+const fs = require('fs')
+const yaml = require('yaml')
+const readPkgUp = require('read-pkg-up')
+
+function parseParamDescriptions(descriptions) {
+    let desc_arr = []
+    for (let desc in descriptions) {
+        desc = descriptions[desc]
+        let desc_obj = {
+            type: desc.replace(/.*{(.*?)}.*/, '$1').trim(),
+            name: desc.replace(/.*\[(.*?)=(.*)\].*/, '$1').trim(),
+            default: desc.replace(/.*\[(.*)=['"]+(.*)['"]+\].*/, '$2').trim()
+        }
+        desc_arr.push(desc_obj)
+    }
+
+    return desc_arr
+}
+
+function describeCommand(command) {
+    let func = path.join(
+        __dirname,
+        command.path_prefix,
+        command.function + '.js'
+    )
+    let name = changeCase.paramCase(command.function)
+    let description = command.description.toLowerCase()
+    let param_arr = []
+    let param_descs = command.parameter_desc
+    param_arr.push(param_descs)
+    param_arr = param_arr.flat()
+    param_descs = param_arr
+
+    param_descs = parseParamDescriptions(param_descs)
+
+    if (description.startsWith('should')) {
+        description = description.substring(7)
+    }
+    description = changeCase.sentenceCase(description)
+    return { param_descs, name, description, func }
+}
+
+async function loadCommands(options) {
+    const yaml_file_data = fs.readFileSync(options.path, 'utf8')
+    const spec = await yaml.parse(yaml_file_data)
+
+    const path_prefix = spec.path_prefix
+    for (let command in spec.api) {
+        command = spec.api[command]
+        command.path_prefix = path_prefix
+        let { param_descs, name, description, func } = describeCommand(command)
+        try {
+            func = require(func)
+        } catch {
+            console.log('failed to load function: ' + func)
+        }
+
+        program
+            .command(name)
+            .description(description)
+            .option(
+                '--no-pretty',
+                'Will print stringified objects instead of colorized yaml'
+            )
+            .action(async options => {
+                let res = await func(options)
+                if (typeof res === 'object' && options.pretty) {
+                    console.log(res)
+                } else {
+                    console.log(res)
+                }
+            })
+
+        for (let param in param_descs) {
+            let idx = program.commands.findIndex(function(o) {
+                return o._name === name
+            })
+            param = param_descs[param]
+            param_name = changeCase.paramCase(param.name)
+            program.commands[idx].option(
+                '--' + param_name + ' <' + param_name + '>',
+                param.name + ' default value is ' + param.default
+            )
+        }
+    }
+
+    const config = await readPkgUp(path.dirname(options.path))
+
+    program.name(config.package.name)
+    program.version(config.package.version)
+
+    return program
+}
+
+module.exports = loadCommands
